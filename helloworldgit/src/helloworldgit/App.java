f293c226a8aeb2714eaa523764837a1b6cd575ef
jm4clark <jamesmclark96@gmail.com>
2019-06-04 14:48:39 +0100

    map

--- helloworldgit/src/helloworldgit/App.java
+++ helloworldgit/src/helloworldgit/App.java
@@ -1,7 +1,25 @@
 package helloworldgit;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello world to the standard out");
+		
+		HashMap<String, String> map = new HashMap<>();
+		
+		addToMap(map, "a", "b");
+		
+		System.out.println(getFromMap(map, "a"));
+	}
+	
+	public static void addToMap(Map<String, String> m, String a, String b) {
+		m.put(a, b);
+	}
+	
+	
+	public static String getFromMap(Map<String, String> m, String a) {
+		return m.get(a);
 	}
 }
